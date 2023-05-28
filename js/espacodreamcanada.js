a63dcc58d3e45c5de47d532b71a7f4fedd686f05
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-05-28 00:18:59 -0300

    criação do javascript para a função de scroll no site

---
+++ js/espacodreamcanada.js
@@ -0,0 +1,14 @@
+function scrollToTurismo() {
+    var turismo = document.getElementById("turismo");
+    window.scrollTo({ top: 900, behavior: 'smooth' });
+  }
+
+  function scrollToIntercambio() {
+    var intercambio = document.getElementById("intercambio");
+    window.scrollTo({ top: 1950, behavior: 'smooth' });
+  }
+
+  function scrollToImigrar() {
+    var imigrar = document.getElementById("imigrar");
+    window.scrollTo({ top: 2950, behavior: 'smooth' });
+  }
