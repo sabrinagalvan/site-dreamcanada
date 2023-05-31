97d298866fb2458ae897a648d57d9d88688e34aa
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-05-31 02:27:24 -0300

    adicionando button switch dark-mode espacodreamcanada.js

--- js/espacodreamcanada.js
+++ js/espacodreamcanada.js
@@ -1,22 +1,22 @@
 function scrollToTurismo() {
-    var turismo = document.getElementById("turismo");
-    window.scrollTo({ top: 900, behavior: 'smooth' });
-  }
+  var turismo = document.getElementById("turismo");
+  window.scrollTo({ top: 900, behavior: 'smooth' });
+}
 
-  function scrollToIntercambio() {
-    var intercambio = document.getElementById("intercambio");
-    window.scrollTo({ top: 1950, behavior: 'smooth' });
-  }
+function scrollToIntercambio() {
+  var intercambio = document.getElementById("intercambio");
+  window.scrollTo({ top: 1950, behavior: 'smooth' });
+}
 
-  function scrollToImigrar() {
-    var imigrar = document.getElementById("imigrar");
-    window.scrollTo({ top: 2950, behavior: 'smooth' });
-  }
+function scrollToImigrar() {
+  var imigrar = document.getElementById("imigrar");
+  window.scrollTo({ top: 2950, behavior: 'smooth' });
+}
 
-const toggleSwitch = document.querySelector('.toggle-switch');
+const themeSwitch = document.getElementById('theme-switch');
+const body = document.body;
 
-toggleSwitch.addEventListener('change', function() {
-  const body = document.querySelector('body');
+themeSwitch.addEventListener('change', function() {
   if (this.checked) {
     body.classList.add('dark-mode');
   } else {
